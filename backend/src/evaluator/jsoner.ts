2c011aeaec0d84c5afb1ce9736c691d64710f743
eiletan <nathanfhlai@gmail.com>
2022-10-15 03:50:31 -0700

    IT'S 4AM AND THERE'S A TYPESCRIPT COMPILER IN MY HOUSE

--- backend/src/evaluator/jsoner.ts
+++ backend/src/evaluator/jsoner.ts
@@ -2,6 +2,12 @@ import { Program } from "../ast/Program";
 import { Entity } from "../ast/Entity";
 import { Entitybody } from "../ast/Entitybody";
 import { Statement } from "../ast/Statement";
+import { Level } from "../ast/Level";
+import { Levelbody } from "../ast/Levelbody";
+import { Levelentity } from "../ast/Levelentity";
+import { Exp } from "../ast/Exp";
+import { Listobject } from "../ast/Listobject";
+import { Player } from "../ast/Player";
 
 export class jsoner {
     private program: Program;
@@ -12,12 +18,19 @@ export class jsoner {
 
     public jsoner(): any {
         // Stub
+        let gameJson: any= {};
+        gameJson["game"] = {};
+        gameJson["game"] = this.entitiesJsoner(this.program.entities);
+        gameJson["game"]["levels"] = this.levelsJsoner(this.program.levels);
+        console.log(gameJson);
+
     }
+    
 
     private entitiesJsoner(entities: Array<Entity>): Object {
         let entitiesJson: any = {};
         for (let entity of entities) {
-            // Cast component and name to string
+            // Cast component and name to string. Component what we call the type
             let type:string  = entity.component as string;
             let entityName: string = entity.name as string;
             let typeBody: any = {};
@@ -26,7 +39,7 @@ export class jsoner {
             for (let s of statements) {
                 typeBody[s.property] = s.value.value;
             }
-            // If type does not already exist in the JSON, create a key for it, else put the entity body in the existing key
+            // If type does not already exist in the JSON, create a key for it, else put the type definition in the existing type object
             if (entitiesJson[type] === undefined) {
                 entitiesJson[type] = {};
                 entitiesJson[type][entityName] = typeBody;
@@ -34,6 +47,97 @@ export class jsoner {
                 entitiesJson[type][entityName] = typeBody;
             }
         }
+        let player: Player = this.program.player;
+        let playerJson: any = {};
+        for (let s of player.body.statements) {
+            playerJson[s.property] = s.value.value; 
+        }
+        entitiesJson[player.name as string] = playerJson;
         return {"types": entitiesJson};
+    }
+
+
+    private levelsJsoner(levels: Array<Level>) {
+        let levelsJson: Array<any> = [];
+        for (let level of levels) {
+            let levelJson: any = {};
+            levelJson["name"] = level.name;
+            let levelBody: Levelbody = level.body;
+            let levelStatements: Array<Statement> = levelBody.statements;
+            for (let ls of levelStatements) {
+                if (ls.property === "player") {
+                    levelJson["playerStartX"] = ls.value.value.exps[0].value;
+                    levelJson["playerStartY"] = ls.value.value.exps[1].value;
+                } else {
+                    levelJson[ls.property] = ls.value.value;
+                }
+                
+            }
+            let levelEntities: Array<Levelentity> = levelBody.entities;
+            let instArr: Array<any> = [];
+            for (let levelEnt of levelEntities) {
+                let type: string = levelEnt.component as string;
+                if (type.toLowerCase() === "terrain" || type.toLowerCase() === "platform") {
+                    let arrs: Array<any> = [];
+                    let levelEntStatements: Array<Statement> = levelEnt.statements;
+                    for (let les of levelEntStatements) {
+                        let listO: Array<Listobject> = les.value.value.listobjects; 
+                        for (let lo of listO) {
+                            let typeJson: any = {};
+                            typeJson["name"] = lo.exps[0].value;
+                            typeJson["type"] = les.property;
+                            typeJson["xPos"] = lo.exps[1].value;
+                            typeJson["yPos"] = lo.exps[2].value;
+                            typeJson["length"] = lo.exps[3].value;
+                            arrs.push(typeJson);
+                        }
+                        if (type.toLowerCase() !== "terrain") {
+                            type = type + "s";
+                        }
+                        levelJson[type] = arrs;
+                    }
+                    
+                } else if (type.toLowerCase() === "door") {
+                    let arrs: Array<any> = [];
+                    let levelEntStatements: Array<Statement> = levelEnt.statements;
+                    for (let les of levelEntStatements) {
+                        let listO: Array<Listobject> = les.value.value.listobjects; 
+                        for (let lo of listO) {
+                            let typeJson: any = {};
+                            typeJson["name"] = lo.exps[0].value;
+                            typeJson["type"] = les.property;
+                            typeJson["goesTo"] = lo.exps[1].value;
+                            typeJson["xPos"] = lo.exps[2].value;
+                            typeJson["yPos"] = lo.exps[3].value;
+                            arrs.push(typeJson);
+                        }
+                        type = type + "s";
+                        levelJson[type] = arrs;
+                    }
+                } else {
+                    let arrs: Array<any> = [];
+                    let levelEntStatements: Array<Statement> = levelEnt.statements;
+                    for (let les of levelEntStatements) {
+                        let listO: Array<Listobject> = les.value.value.listobjects; 
+                        for (let lo of listO) {
+                            let typeJson: any = {};
+                            typeJson["name"] = lo.exps[0].value;
+                            typeJson["type"] = les.property;
+                            typeJson["xPos"] = lo.exps[1].value;
+                            typeJson["yPos"] = lo.exps[2].value;
+                            arrs.push(typeJson);
+                        }
+                        if (type.toLowerCase() === "enemy") {
+                            type = "Enemies";
+                        } else {
+                            type = type + "s";
+                        }
+                        levelJson[type] = arrs;
+                    }
+                }
+            }
+            levelsJson.push(levelJson);
+        }
+        return levelsJson;
     }
 }
